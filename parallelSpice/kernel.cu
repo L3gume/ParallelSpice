ef028956acf01e8ed52e1e40a0e4edb4241101bb
Evan Laflamme <evan.laflamme@mail.mcgill.ca>
2019-11-21 13:24:17 -0500

    Implement B generation and solver. TODO: Test.

--- parallelSpice/kernel.cu
+++ parallelSpice/kernel.cu
@@ -9,65 +9,68 @@
 #include <iostream>
 #include "JsonParser.h"
 #include "dbg.h"
+#include "cuda_utils.h"
 
 cudaError_t solve(json::parse_result& data);
-void choleskyDecomposition(std::vector<double> A, std::vector<double> L, const int dim);
-double* computeCoefficientMatrix(cuda_mem::grid<double> A, std::vector<double> Y, const int dim);
+std::vector<double> choleskyDecomposition(std::vector<double> S, const int n);
+std::vector<double> computeCoefficientMatrix(std::vector<std::vector<double>> A, std::vector<double> Y, const int n, const int m);
 
-__global__ void choleskyTimepointSolverKernel(double* A, double* L, double* bs, double* y, double* xs, const int dim) {
+// Returns X_is where Xs are the node voltages associated with different timepoints
+__global__ void choleskyTimepointSolverKernel(double* L, double* Bs, double* temp_solver, double* Xs, const int n) {
 	const auto time_point = blockIdx.x * blockDim.x + threadIdx.x;
-	const auto b = &bs[time_point * dim];
+	const auto b = &Bs[time_point * n];
 	auto accumulator = 0.0;
 
-	// Reset y to 0
-	for (auto i = 0; i < dim; i++)
+	// Ensure temp is 0
+	/*for (auto i = 0; i < n; i++)
 	{
-		y[i] = 0.0;
-	}
+		temp[i] = 0.0;
+	}*/
 
-	// Solve Ly = b
-	for (auto i = 0; i < dim; i++)
+	// Solve L*temp_solver = b
+	for (auto i = 0; i < n; i++)
 	{
 		accumulator = 0.0;
 
 		for (auto j = 0; j < i; j++)
 		{
-			accumulator += L[i * dim + j] * y[j];
+			accumulator += L[i * n + j] * temp_solver[j];
 		}
 
-		y[i] = (b[i] - accumulator) / L[i * dim + i];
+		temp_solver[i] = (b[i] - accumulator) / L[i * n + i];
 	}
 
-	// Solve (L_T)x = y
-	for (auto i = dim - 1; i < 0; i--)
+	// Solve (L_T)x = temp
+	for (auto i = n - 1; i < 0; i--)
 	{
 		accumulator = 0.0;
-		for (auto j = i + 1; j < dim; j++)
+		for (auto j = i + 1; j < n; j++)
 		{
-			accumulator += L[j * dim + i] * xs[time_point * dim + j];
+			accumulator += L[j * n + i] * Xs[time_point * n + j];
 		}
 		
-		xs[time_point * dim + i] = (y[i] - accumulator) / L[i * dim + i];
+		Xs[time_point * n + i] = (temp_solver[i] - accumulator) / L[i * n + i];
 	}
 }
 
-__global__ void bTimepointGeneratorKernel(double* A, double* Bs, double* Y, double* J, double* E, double* F, double* temp, const int dim, const int time_slice) {
+// Returns a set of Bs where B_i is the forcing function at a timepoint
+__global__ void bTimepointGeneratorKernel(double* A, double* Bs, double* Y, double* J, double* E, double* F, double* temp_generator, const int n, const int m, const int time_slice) {
 	const auto time_point = blockIdx.x * blockDim.x + threadIdx.x;
-	const auto B = &Bs[time_point * dim];
+	const auto B = &Bs[time_point * n];
 	
 	// Calculate J - YE
-	for (auto i = 0; i < dim; i++)
+	for (auto i = 0; i < m; i++)
 	{
-		temp[i] = J[i] - Y[i] * E[i] * cos(F[i] * time_point / time_slice);
+		temp_generator[i] = J[i] - Y[i] * E[i] * cos(F[i] * time_point / time_slice);
 	}
 
 	// Multiply A by output
-	for (auto i = 0; i < dim; i++)
+	for (auto i = 0; i < n; i++)
 	{
 		auto sum = 0.0;
-		for (auto j = 0; j < dim; j++)
+		for (auto j = 0; j < m; j++)
 		{
-			sum += A[i * dim + j] * temp[j];
+			sum += A[i * n + j] * temp_generator[j];
 		}
 
 		// Store in B
@@ -76,58 +79,61 @@ __global__ void bTimepointGeneratorKernel(double* A, double* Bs, double* Y, doub
 }
 
 // Perform Cholesky decomposition
-void choleskyDecomposition(std::vector<double> A, std::vector<double> L, const int dim) {
+std::vector<double> choleskyDecomposition(std::vector<double> S, const int n) {
+	std::vector<double> L(n * n, 0.0);
 	auto accumulator = 0.0;
 
-	for (auto i = 0; i < dim; i++)
+	for (auto i = 0; i < n; i++)
 	{
 		for (auto j = 0; j < (i + 1); j++)
 		{
 			accumulator = 0.0;
 			for (auto k = 0; k < j; k++)
 			{
-				accumulator += L[i * dim + k] * L[j * dim + k];
+				accumulator += L[i * n + k] * L[j * n + k];
 			}
 
 			if (i == j)
 			{
-				L[i * dim + j] = sqrt(A[i * dim + i] - accumulator);
+				L[i * n + j] = sqrt(S[i * n + i] - accumulator);
 			}
 			else
 			{
-				L[i * dim + j] = (1.0 / L[j * dim + j]) * (A[i * dim + j] - accumulator);
+				L[i * n + j] = (1.0 / L[j * n + j]) * (S[i * n + j] - accumulator);
 			}
 		}
 	}
+
+	return L;
 }
 
-double* computeCoefficientMatrix(cuda_mem::grid<double> A, std::vector<double> Y, const int dim)
+std::vector<double> computeCoefficientMatrix(std::vector<std::vector<double>> A, std::vector<double> Y, const int n, const int m)
 {
-	auto temp = new double[dim * dim];
+	std::vector<double> temp(m * n, 0.0);
 
 	// Multiply A by Y
-	for (auto i = 0; i < dim; i++)
+	for (auto i = 0; i < m; i++)
 	{
-		for (auto j = 0; j < dim; j++)
+		for (auto j = 0; j < n; j++)
 		{
-			temp[i * dim + j] = A[i][j] * Y[j];
+			temp[i * m + j] = A[i][j] * Y[j];
 		}
 	}
 
-	auto output = new double[dim * dim];
+	std::vector<double> output(n * n, 0.0);
 
 	// Multiply Y by transpose of A
-	for (auto i = 0; i < dim; i++)
+	for (auto i = 0; i < m; i++)
 	{
-		for (auto j = 0; j < dim; j++)
+		for (auto j = 0; j < n; j++)
 		{
 			auto sum = 0.0;
-			for (auto k = 0; k < dim; k++)
+			for (auto k = 0; k < m; k++)
 			{
-				sum += A[i][j] * temp[i * dim + j];
+				sum += A[k][i] * temp[k * m + j];
 			}
 
-			output[i * dim + j] = sum;
+			output[i * n + j] = sum;
 		}
 	}
 
@@ -168,35 +174,35 @@ int main(const int argc, char* argv[]) {
 }
 
 cudaError_t solve(json::parse_result& data) {
-	auto num_timepoints = 10 * static_cast<int>(*std::max_element(data.F.data(), data.F.data() + data.F.size())); // Set num_timepoints as 10*MAX(F)
-	auto A = data.A;
-	std::vector<double> L(data.n * data.n, 0.0);
-	std::vector<double> Bs(data.timepoints * data.n, 10.0);
-	std::vector<double> Xs(data.timepoints * data.n, 0.0);
+	auto num_timepoints = 10 * std::max(1, static_cast<int>(*std::max_element(data.F.data(), data.F.data() + data.F.size()))); // Set num_timepoints as 10*MAX(F)
+	std::vector<double> Bs(num_timepoints * data.n, 10.0);
+	std::vector<double> Xs(num_timepoints * data.n, 0.0);
+	std::vector<double> temp_solver(data.n, 0.0);
+	std::vector<double> temp_generator(data.n, 0.0);
+
+	/*auto grid_device = cuda_mem::grid_to_cuda_2d(data.A);
+	auto g = cuda_mem::cuda_2d_to_grid(grid_device);
+
+	auto vec_device = cuda_mem::make_cuda_unique(data.E);
+	auto vec = cuda_mem::cuda_unique_to_vector(vec_device);*/
 
 	double* dev_A = 0;
+	double* dev_S = 0;
 	double* dev_L = 0;
 	double* dev_Bs = 0;
 	double* dev_Xs = 0;
 	double* dev_Y = 0;
+	double* dev_J = 0;
+	double* dev_E = 0;
+	double* dev_F = 0;
+	double* dev_temp_solver = 0;
+	double* dev_temp_generator = 0;
 
 	// Get coefficient matrix
-	auto A = computeCoefficientMatrix(*A.data(), data.Y, data.n);
-	
-	// Perform Cholesky decomposition
-	choleskyDecomposition(*A.data(), L, data.n);
-
-	// Print timepoints
-	printf("L: \n");
-	for (auto i = 0; i < data.n; i++)
-	{
+	auto S = computeCoefficientMatrix(data.A, data.Y, data.n, data.m);
 		
-		for (auto j = 0; j < data.n; j++)
-		{
-			printf("%lf ", L[i * data.n + j]);
-		}
-		printf("\n");
-	}
+	// Perform Cholesky decomposition
+	auto L = choleskyDecomposition(S, data.n);
 
 	// Choose which GPU to run on, change this on a multi-GPU system.
 	auto cudaStatus = cudaSetDevice(0);
@@ -206,9 +212,15 @@ cudaError_t solve(json::parse_result& data) {
 	}
 
 	// Allocate GPU buffers for image and new image.
-	cudaStatus = cudaMalloc((void**)& dev_A, data.n * data.n * sizeof(double));
+	cudaStatus = cudaMalloc((void**)& dev_A, data.m * data.n * sizeof(double));
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc for A array failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMalloc((void**)& dev_S, data.n * data.n * sizeof(double));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc for S array failed!");
 		goto Error;
 	}
 
@@ -230,16 +242,61 @@ cudaError_t solve(json::parse_result& data) {
 		goto Error;
 	}
 
-	cudaStatus = cudaMalloc((void**)& dev_Y, data.n * sizeof(double));
+	cudaStatus = cudaMalloc((void**)& dev_Y, data.m * sizeof(double));
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMalloc for Y array failed!");
 		goto Error;
 	}
 
+	cudaStatus = cudaMalloc((void**)& dev_J, data.m * sizeof(double));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc for J array failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMalloc((void**)& dev_E, data.m * sizeof(double));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc for E array failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMalloc((void**)& dev_F, data.m * sizeof(double));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc for F array failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMalloc((void**)& dev_temp_solver, data.n * sizeof(double));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc for temp solver array failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMalloc((void**)& dev_temp_generator, data.n * sizeof(double));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc for temp solver array failed!");
+		goto Error;
+	}
+
+	auto a = new double[data.m * data.n];
+	for (auto i = 0; i < data.n; i++)
+	{
+		for (auto j = 0; j < data.m; j++)
+		{
+			a[i * data.m + j] = data.A[i][j];
+		}
+	}
+
 	// Copy input vectors from host memory to GPU buffers.
-	cudaStatus = cudaMemcpy(dev_A, A.data(), data.n * data.n * sizeof(double), cudaMemcpyHostToDevice);
+	cudaStatus = cudaMemcpy(dev_A, a, data.m * data.n * sizeof(double), cudaMemcpyHostToDevice);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMemcpy from A to dev_A failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMemcpy(dev_S, S.data(), data.n * data.n * sizeof(double), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy from S to dev_S failed!");
 		goto Error;
 	}
 
@@ -261,14 +318,73 @@ cudaError_t solve(json::parse_result& data) {
 		goto Error;
 	}
 
-	cudaStatus = cudaMemcpy(dev_Y, data.Y.data(), data.n * sizeof(double), cudaMemcpyHostToDevice);
+	cudaStatus = cudaMemcpy(dev_Y, data.Y.data(), data.m * sizeof(double), cudaMemcpyHostToDevice);
 	if (cudaStatus != cudaSuccess) {
 		fprintf(stderr, "cudaMemcpy from Y to dev_Y failed!");
 		goto Error;
 	}
 
+	cudaStatus = cudaMemcpy(dev_J, data.J.data(), data.m * sizeof(double), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy from J to dev_J failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMemcpy(dev_E, data.E.data(), data.m * sizeof(double), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy from E to dev_E failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMemcpy(dev_F, data.F.data(), data.m * sizeof(double), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy from F to dev_F failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMemcpy(dev_temp_solver, temp_solver.data(), data.n * sizeof(double), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy from temp_solver to dev_temp_solver failed!");
+		goto Error;
+	}
+
+	cudaStatus = cudaMemcpy(dev_temp_generator, temp_generator.data(), data.n * sizeof(double), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy from temp_generator to dev_temp_generator failed!");
+		goto Error;
+	}
+
+	int num_blocks = 0;
+	int num_threads = 0;
+	cuda_utils::divide_threads_into_blocks(num_timepoints, num_blocks, num_threads);
+
+	// Generate Bs
+	bTimepointGeneratorKernel << <num_blocks, num_threads >> > (dev_A, dev_Bs, dev_Y, dev_J, dev_E, dev_F, dev_temp_generator, data.n, data.m, num_timepoints);
+
+	// Check for any errors launching the kernel
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "bTimepointGeneratorKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		goto Error;
+	}
+
+	// cudaDeviceSynchronize waits for the kernel to finish, and returns
+	// any errors encountered during the launch.
+	cudaStatus = cudaDeviceSynchronize();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching bTimepointGeneratorKernel!\n", cudaStatus);
+		goto Error;
+	}
+
+	// Copy output vector from GPU buffer to host memory.
+	cudaStatus = cudaMemcpy(Bs.data(), dev_Bs, num_timepoints * data.n * sizeof(double), cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy from dev_Bs to Bs failed!");
+		goto Error;
+	}
+
 	// Perform solver for each timepoint
-	choleskyTimepointSolverKernel << <1, num_timepoints >> > (dev_A, dev_L, dev_Bs, dev_Y, dev_Xs, data.n);
+	choleskyTimepointSolverKernel<<<num_blocks, num_threads >>>(dev_L, dev_Bs, dev_temp_solver, dev_Xs, data.n);
 
 	// Check for any errors launching the kernel
 	cudaStatus = cudaGetLastError();
@@ -305,9 +421,16 @@ cudaError_t solve(json::parse_result& data) {
 
 Error:
 	cudaFree(dev_A);
+	cudaFree(dev_S);
 	cudaFree(dev_L);
 	cudaFree(dev_Bs);
 	cudaFree(dev_Xs);
+	cudaFree(dev_Y);
+	cudaFree(dev_J);
+	cudaFree(dev_E);
+	cudaFree(dev_F);
+	cudaFree(dev_temp_solver);
+	cudaFree(dev_temp_generator);
 
 	return cudaStatus;
 }
