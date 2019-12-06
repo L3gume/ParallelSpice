a01930c11c9a9811f2b70c50e7626a75ef0d9042
Chloe Grosdidier <jawaialler@gmail.com>
2019-12-06 18:19:28 -0500

    Correct values in mesh sample creator

--- scripts/meshSampleCreator.py
+++ scripts/meshSampleCreator.py
@@ -24,7 +24,7 @@ data['N'] = len(data['A'][0])
 data['Y'] = [0.1, 0.1]
 data['E'] = [0,0]
 data['J'] = [10,0]
-data['VF'] = [0,0]
+data['VF'] = [0,6]
 data['IF'] = [10,10]
 
 with open(json_directory_path + 'test_2' + '.json', 'w') as f:
@@ -37,8 +37,8 @@ data['N'] = len(data['A'][0])
 data['Y'] = [0.1, 0.1]
 data['E'] = [10,0]
 data['J'] = [0,10]
-data['VF'] = [10,10]
-data['IF'] = [5,5]
+data['VF'] = [19,10]
+data['IF'] = [7,5]
 
 with open(json_directory_path + 'test_3' + '.json', 'w') as f:
     json.dump(data, f)
@@ -50,8 +50,8 @@ data['N'] = len(data['A'][0])
 data['Y'] = [0.1, 0.1, 0.2, 0.2]
 data['E'] = [10,0,0,0]
 data['J'] = [0,0,0,10]
-data['VF'] = [3,3,3,3]
-data['IF'] = [7,7,7,7]
+data['VF'] = [3,8,0.5,2]
+data['IF'] = [0.9,4.9,0,2]
 
 with open(json_directory_path + 'test_4' + '.json', 'w') as f:
     json.dump(data, f)
@@ -63,8 +63,8 @@ data['N'] = len(data['A'][0])
 data['Y'] = [0.05, 0.1, 0.033, 0.033, 0.1, 0.033]
 data['E'] = [10,0,0,0,0,0]
 data['J'] = [0,0,0,0,0,0]
-data['VF'] = [5,5,5,5,5,5]
-data['IF'] = [0,0,0,0,0,0]
+data['VF'] = [5,4,5,1,8,1.5]
+data['IF'] = [0,1,3,0,3,2]
 
 with open(json_directory_path + 'test_5' + '.json', 'w') as f:
     json.dump(data, f)
